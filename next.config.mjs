73345faad46ca54e858804445f7f8361b368c7ca
lennythecreator <lenyeuwaeme@gmail.com>
2025-09-07 01:40:19 -0400

    fix canvas build error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
     images: {
         domains: ['books.google.com'], // Add the domain here
       },
+      webpack: (config) => {
+    config.module.rules.push({
+      test: /canvas\.node$/,
+      use: 'file-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
